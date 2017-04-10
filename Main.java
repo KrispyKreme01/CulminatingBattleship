9c1821a187c07cd0838debf9ff46966fdc0c363f
KrispyKreme01 <krmalatrash@gmail.com>
2017-04-10 15:24:47 -0400

    Made Changles

--- Main.java
+++ Main.java
@@ -3,39 +3,55 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		boolean[][] enemyBoard = new boolean[9][9];
-		boolean[][] friendlyBoard = new boolean[9][9];
-		  friendlyBoardChooser(friendlyBoard);
-	      friendlyBoardMaker(friendlyBoard);
+		int[][] enemyBoard = new int[9][9];
+		//boolean[][] friendlyBoard = new boolean[9][9];
+		 // friendlyBoardChooser(friendlyBoard);
+	     // friendlyBoardMaker(friendlyBoard);
+	      enemyBoardMaker(enemyBoard);
 	       }
 
 	public static void friendlyBoardChooser(boolean[][] friendlyBoard) //this chooses the coordinates for the user
 	{
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Please enter your three coordinates");
+		System.out.println("Please enter your five coordinates");
 		for (int coordinateAmount = 1; coordinateAmount <= 5; coordinateAmount++){
 			System.out.println("X-value of coordinate number " + coordinateAmount + ":");
 			int coordinateNumberX = sc.nextInt();
 			System.out.println("Y-value of coordinate number " + coordinateAmount + ":");
 			int coordinateNumberY = sc.nextInt();
 			friendlyBoard[coordinateNumberX][coordinateNumberY] = true;
+			
 		}
 	}
 		public static void friendlyBoardMaker(boolean[][] friendlyBoard) //this shows the users board
 	{
 			System.out.println("\t0 \t1 \t2 \t3 \t4 \t5 \t6 \t7 \t8");
-		for(int row1=0 ; row1 < 8 ; row1++ ){
+		for(int row1=0 ; row1 < 9 ; row1++ ){
             System.out.print("\n"+(row1)); 
             for(int column1=0 ; column1 < 9 ; column1++ ){
                 if(friendlyBoard[row1][column1] != true)
                     System.out.print("\t"+"~");
                 
                 else if(friendlyBoard[row1][column1])
-                    System.out.print("\t"+"X" + "DICKS");
+                    System.out.print("\t"+"X");
                 }
 
     }
 	}
+		public static void enemyBoardMaker (int[][] enemyBoard) {
+			enemyBoard[1][1] = true;
+			for(int row1=0 ; row1 < 9 ; row1++ ){
+	            System.out.print("\n"+(row1)); 
+	            for(int column1=0 ; column1 < 9 ; column1++ ){
+	                if(enemyBoard[row1][column1] != true)
+	                    System.out.print("\t"+"~");
+	                
+	                else if(enemyBoard[row1][column1])
+	                    System.out.print("\t"+"~");
+	                }
+
+	    }
+		}
 	 /*public static void friendlyBoard(int[][] board){
 	        System.out.println("\t1 \t2 \t3 \t4 \t5");
 	        System.out.println();
