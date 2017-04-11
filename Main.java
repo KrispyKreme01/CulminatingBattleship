266e1a2839a2b75364dbdc686a259a0dab7d3d84
KrispyKreme01 <krmalatrash@gmail.com>
2017-04-11 11:31:45 -0400

    + Enemy Board Maker

--- Main.java
+++ Main.java
@@ -1,72 +1,71 @@
-
+import java.util.Random;
 import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
 		int[][] enemyBoard = new int[9][9];
-		//boolean[][] friendlyBoard = new boolean[9][9];
-		 // friendlyBoardChooser(friendlyBoard);
-	     // friendlyBoardMaker(friendlyBoard);
-	      enemyBoardMaker(enemyBoard);
-	       }
+		boolean[][] friendlyBoard = new boolean[9][9];
+		friendlyBoardChooser(friendlyBoard);
+		friendlyBoardMaker(friendlyBoard);
+		enemyBoardMaker(enemyBoard);
+	}
 
 	public static void friendlyBoardChooser(boolean[][] friendlyBoard) //this chooses the coordinates for the user
 	{
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Please enter your five coordinates");
-		for (int coordinateAmount = 1; coordinateAmount <= 5; coordinateAmount++){
+		for (int coordinateAmount = 1; coordinateAmount <= 5; coordinateAmount++) {
 			System.out.println("X-value of coordinate number " + coordinateAmount + ":");
 			int coordinateNumberX = sc.nextInt();
 			System.out.println("Y-value of coordinate number " + coordinateAmount + ":");
 			int coordinateNumberY = sc.nextInt();
 			friendlyBoard[coordinateNumberX][coordinateNumberY] = true;
-			
+
 		}
 	}
-		public static void friendlyBoardMaker(boolean[][] friendlyBoard) //this shows the users board
+
+	public static void friendlyBoardMaker(boolean[][] friendlyBoard) //this shows the users board
 	{
-			System.out.println("\t0 \t1 \t2 \t3 \t4 \t5 \t6 \t7 \t8");
-		for(int row1=0 ; row1 < 9 ; row1++ ){
-            System.out.print("\n"+(row1)); 
-            for(int column1=0 ; column1 < 9 ; column1++ ){
-                if(friendlyBoard[row1][column1] != true)
-                    System.out.print("\t"+"~");
-                
-                else if(friendlyBoard[row1][column1])
-                    System.out.print("\t"+"X");
-                }
+		System.out.println("\t0 \t1 \t2 \t3 \t4 \t5 \t6 \t7 \t8");
+		for (int row1 = 0; row1 < 9; row1++) {
+			System.out.print("\n" + (row1));
+			for (int column1 = 0; column1 < 9; column1++) {
+				if (friendlyBoard[row1][column1] != true)
+					System.out.print("\t" + "~");
 
-    }
-	}
-		public static void enemyBoardMaker (int[][] enemyBoard) {
-			enemyBoard[1][1] = true;
-			for(int row1=0 ; row1 < 9 ; row1++ ){
-	            System.out.print("\n"+(row1)); 
-	            for(int column1=0 ; column1 < 9 ; column1++ ){
-	                if(enemyBoard[row1][column1] != true)
-	                    System.out.print("\t"+"~");
-	                
-	                else if(enemyBoard[row1][column1])
-	                    System.out.print("\t"+"~");
-	                }
+				else if (friendlyBoard[row1][column1])
+					System.out.print("\t" + "X");
+			}
 
-	    }
 		}
-	 /*public static void friendlyBoard(int[][] board){
-	        System.out.println("\t1 \t2 \t3 \t4 \t5");
-	        System.out.println();
-	        
-	        for(int row=0 ; row < 5 ; row++ ){
-	            System.out.print((row+1)+""); 
-	            for(int column=0 ; column < 5 ; column++ ){
-	                if(friendlyBoard[row][column] != true)
-	                    System.out.print("\t"+"~");
-	                
-	                else if(friendlyBoard[row][column])
-	                    System.out.print("\t"+"X");
-	                }
+	}
 
-	    } */
-	 }
+	public static void enemyBoardMaker(int[][] enemyBoard) {
+		Random rand = new Random();
+		for (int i = 0; i < 5; i++) {
+			//sets up enemy battle ship spots
+			int enemyCoordX = rand.nextInt(8);
+			int enemyCoordY = rand.nextInt(8);
+			enemyBoard[enemyCoordX][enemyCoordY] = 1;
+
+		}
+		for (int row1 = 0; row1 < 9; row1++) {
+			System.out.print("\n" + (row1));
+			for (int column1 = 0; column1 < 9; column1++) {
+				if (enemyBoard[row1][column1] == 0)
+					System.out.print("\t" + "~");
+
+				else if (enemyBoard[row1][column1] == 1)
+					System.out.print("\t" + "~");
+
+				else if (enemyBoard[row1][column1] == 2)
+					System.out.print("\t" + "X");
+			}
+
+
+		}
+	}
+}
+
 
 
