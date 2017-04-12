86bfbfe91a40d28059e9e13f179c9ed95d4c088f
KrispyKreme01 <krmalatrash@gmail.com>
2017-04-12 12:13:42 -0400

    +if statement to prevent duplicates *need to figure out why the row numbers are printing twice

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		int[][] enemyBoard = new int[9][9];
-		boolean[][] friendlyBoard = new boolean[9][9];
+		int[][] enemyBoard = new int[6][6];
+		boolean[][] friendlyBoard = new boolean[6][6];
 		friendlyBoardChooser(friendlyBoard);
 		friendlyBoardMaker(friendlyBoard);
 		enemyBoardMaker(enemyBoard);
@@ -26,10 +26,10 @@ public class Main {
 
 	public static void friendlyBoardMaker(boolean[][] friendlyBoard) //this shows the users board
 	{
-		System.out.println("\t0 \t1 \t2 \t3 \t4 \t5 \t6 \t7 \t8");
-		for (int row1 = 0; row1 < 9; row1++) {
+		System.out.println("\t0 \t1 \t2 \t3 \t4 \t5");
+		for (int row1 = 0; row1 < 6; row1++) {
 			System.out.print("\n" + (row1));
-			for (int column1 = 0; column1 < 9; column1++) {
+			for (int column1 = 0; column1 < 6; column1++) {
 				if (friendlyBoard[row1][column1] != true)
 					System.out.print("\t" + "~");
 
@@ -44,14 +44,18 @@ public class Main {
 		Random rand = new Random();
 		for (int i = 0; i < 5; i++) {
 			//sets up enemy battle ship spots
-			int enemyCoordX = rand.nextInt(8);
-			int enemyCoordY = rand.nextInt(8);
+			
+			int enemyCoordX = rand.nextInt(6);
+			int enemyCoordY = rand.nextInt(6);
+			if(enemyBoard[enemyCoordX][enemyCoordY] == 0)
 			enemyBoard[enemyCoordX][enemyCoordY] = 1;
+			else
+			i--;
 
 		}
-		for (int row1 = 0; row1 < 9; row1++) {
+		for (int row1 = 0; row1 < 6; row1++) {
 			System.out.print("\n" + (row1));
-			for (int column1 = 0; column1 < 9; column1++) {
+			for (int column1 = 0; column1 < 6; column1++) {
 				if (enemyBoard[row1][column1] == 0)
 					System.out.print("\t" + "~");
 
