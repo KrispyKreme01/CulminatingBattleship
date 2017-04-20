9261efd87720d666214808a667db4324fe5657c9
KrispyKreme01 <krmalatrash@gmail.com>
2017-04-20 14:04:41 -0400

    +testing class

---
+++ TestingClass.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+public class TestingClass {
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int[] array = 
+			int choice = rand(5);
+	}
+
+}
